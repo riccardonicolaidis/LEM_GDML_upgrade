1a4297992b28929d547c601fda5975d070b486cc
riccardo <98612744+riccardonicolaidis@users.noreply.github.com>
2023-06-06 17:56:06 +0200

    Aggiungo i gdml

---
+++ PythonMacros/Analysis.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import sys
+import time
+import subprocess
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import ROOT as rt
+
+
+def Analysis():
+    pass
+
+if __name__ == "__main__":
+    Analysis()
