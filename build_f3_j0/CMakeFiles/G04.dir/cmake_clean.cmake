ccb1ed32a2707c21749cdcb1bc5e55269ff21356
Riccardo Nicolaidis <rnicolai@ams.tifpa.infn.it>
2023-08-07 21:14:19 +0200

    aggiornamenti

--- build_f3_j0/CMakeFiles/G04.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/G04"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/G04.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
